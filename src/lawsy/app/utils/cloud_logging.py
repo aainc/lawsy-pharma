a175c69963d4748130d1c607ed360c3de4b52b5b
Tatsuya Shirakawa <lilys1204@gmail.com>
2025-02-17 23:31:40 +0900

    support GOOGLE_APPLICATION_CREDENTIALS or sa.json (#99)

--- src/lawsy/app/utils/cloud_logging.py
+++ src/lawsy/app/utils/cloud_logging.py
@@ -1,4 +1,12 @@
+import os
+from pathlib import Path
+
 from google.cloud.logging import Client
 
-logging_client = Client()
+key_file = os.getenv("GOOGLE_APPLICATION_CREDENTIALS") or "sa.json"
+
+if Path(key_file).exists():
+    logging_client = Client.from_service_account_json(key_file)
+else:
+    logging_client = Client()
 gcp_logger = logging_client.logger("lawsy-gcp-logger")
